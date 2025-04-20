485c83d2b428bbac341687257d251c1adfc894c8
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-20 16:19:08 +0600

    complete THomeCategories TVerticalImageText and TSectionHeading

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -12,34 +12,39 @@ class TSearchContainer extends StatelessWidget {
     this.icon = Iconsax.search_normal,
     this.showBackground = true,
     this.showBorder = true,
+    this.onTap,
   });
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context),
-        padding: EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground
-              ? dark
-                  ? TColors.dark
-                  : TColors.light
-              : Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: TColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: TColors.darkerGrey),
-            SizedBox(width: TSizes.spaceBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                    ? TColors.dark
+                    : TColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: TColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: TColors.darkerGrey),
+              SizedBox(width: TSizes.spaceBtwItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/sizes.dart';
-import 'package:t_store/utils/device/device_utility.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -29,7 +28,24 @@ class HomeScreen extends StatelessWidget {
                 TSearchContainer(text: 'Search in Store'),
                 SizedBox(height: TSizes.spaceBtwSections),
 
-                /// Categories:
+                /// --- Categories section:
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      /// --- Heading:
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: TColors.white,
+                      ),
+                      SizedBox(height: TSizes.spaceBtwItems),
+
+                      /// Categories:
+                      THomeCategories(),
+                    ],
+                  ),
+                )
               ],
             ))
           ],
