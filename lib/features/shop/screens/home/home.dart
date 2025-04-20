f644138946ea5e7d72a6f09cdd3135660385283c
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-20 14:53:55 +0600

    complete TSearchContainer

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,8 +23,12 @@ class HomeScreen extends StatelessWidget {
               children: [
                 /// --- Appbar ---
                 THomeAppbar(),
+                SizedBox(height: TSizes.spaceBtwSections),
 
                 /// -- searchbar:
+                TSearchContainer(text: 'Search in Store'),
+                SizedBox(height: TSizes.spaceBtwSections),
+
                 /// Categories:
               ],
             ))
