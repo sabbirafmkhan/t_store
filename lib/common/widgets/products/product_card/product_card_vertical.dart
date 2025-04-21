03a2db36c333c0595213d98ec164e5b2ef0511aa
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-04-21 15:15:01 +0600

    complete TGridLayout

--- lib/common/widgets/products/product_card/product_card_vertical.dart
+++ lib/common/widgets/products/product_card/product_card_vertical.dart
@@ -4,6 +4,8 @@ import 'package:t_store/common/styles/shadows.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
 import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/common/widgets/texts/product_title_text.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
@@ -17,64 +19,118 @@ class TProductCardVertical extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
 
     /// Container with side paddings,color,edges, radius and shadow:
-    return Container(
-      width: 180,
-      padding: EdgeInsets.all(1),
-      decoration: BoxDecoration(
-        boxShadow: [TShadowStyle.verticalProductShadow],
-        borderRadius: BorderRadius.circular(TSizes.productImageRadius),
-        color: dark ? TColors.darkerGrey : TColors.white,
-      ),
-      child: Column(
-        children: [
-          /// Thumbnails , wishlist button, discount tag:
-          TRoundedContainer(
-            height: 180,
-            padding: EdgeInsets.all(TSizes.sm),
-            backgroundColor: dark ? TColors.dark : TColors.light,
-            child: Stack(
-              children: [
-                /// --Thumbnails image:
-                TRoundedImage(
-                  imageUrl: TImages.productImage1,
-                  applyImageRadius: true,
-                ),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 180,
+        padding: EdgeInsets.all(1),
+        decoration: BoxDecoration(
+          boxShadow: [TShadowStyle.verticalProductShadow],
+          borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+          color: dark ? TColors.darkerGrey : TColors.white,
+        ),
+        child: Column(
+          children: [
+            /// Thumbnails , wishlist button, discount tag:
+            TRoundedContainer(
+              height: 180,
+              padding: EdgeInsets.all(TSizes.sm),
+              backgroundColor: dark ? TColors.dark : TColors.light,
+              child: Stack(
+                children: [
+                  /// --Thumbnails image:
+                  TRoundedImage(
+                    imageUrl: TImages.productImage1,
+                    applyImageRadius: true,
+                  ),
 
-                /// -- Sale tag:
-                Positioned(
-                  top: 12,
-                  child: TRoundedContainer(
-                    radius: TSizes.sm,
-                    backgroundColor: TColors.secondary.withOpacity(0.8),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: TSizes.sm,
-                      vertical: TSizes.xs,
-                    ),
-                    child: Text(
-                      '25%',
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelLarge!
-                          .apply(color: TColors.black),
+                  /// -- Sale tag:
+                  Positioned(
+                    top: 12,
+                    child: TRoundedContainer(
+                      radius: TSizes.sm,
+                      backgroundColor: TColors.secondary.withOpacity(0.8),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: TSizes.sm,
+                        vertical: TSizes.xs,
+                      ),
+                      child: Text(
+                        '25%',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge!
+                            .apply(color: TColors.black),
+                      ),
                     ),
                   ),
-                ),
 
-                /// -- Favourite icon button:
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: TCircularIcon(
-                    icon: Iconsax.heart5,
-                    color: Colors.red,
+                  /// -- Favourite icon button:
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: TCircularIcon(
+                      icon: Iconsax.heart5,
+                      color: Colors.red,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
 
-          ///  --- Details:
-        ],
+            ///  --- Details:
+            Padding(
+              padding: EdgeInsets.only(left: TSizes.sm),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TProductTitleText(
+                      title: 'Green NIKE Air Shoes', smallSize: true),
+                  SizedBox(height: TSizes.spaceBtwItems / 2),
+                  Row(
+                    children: [
+                      Text(
+                        'Nike',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                      SizedBox(
+                        height: TSizes.sm,
+                      ),
+                      Icon(Iconsax.verify5,
+                          color: TColors.primary, size: TSizes.iconXs),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// - Price:
+                      TProductPriceText(price: '35.0',),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: TColors.dark,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(TSizes.cardRadiusMd),
+                            bottomRight:
+                                Radius.circular(TSizes.productImageRadius),
+                          ),
+                        ),
+                        child: SizedBox(
+                          width: TSizes.iconLg * 1.2,
+                          height: TSizes.iconLg * 1.2,
+                          child: Center(
+                            child: Icon(Iconsax.add, color: TColors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/layouts/grid_layout.dart';
 import 'package:t_store/common/widgets/products/product_card/product_card_vertical.dart';
 import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
@@ -68,7 +69,10 @@ class HomeScreen extends StatelessWidget {
                   ),
 
                   /// -- Popular Products :
-                  TProductCardVertical(),
+                  TGridLayout(
+                    itemCount: 2,
+                    itemBuilder: (_, index) => TProductCardVertical(),
+                  ),
                 ],
               ),
             )
