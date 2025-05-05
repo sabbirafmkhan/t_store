101db8127ad2b51d8b31988ebbb990663501dec2
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-05 14:17:42 +0600

    CartScreen some changes

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
-import 'package:t_store/common/widgets/products/cart/add_remove_button.dart';
-import 'package:t_store/common/widgets/products/cart/cart_item.dart';
-import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/features/shop/screens/cart/widgets/cart_items.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 class CartScreen extends StatelessWidget {
@@ -20,34 +18,12 @@ class CartScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(TSizes.defaultSpace),
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemCount: 15,
-          separatorBuilder: (_, __) =>
-              SizedBox(height: TSizes.spaceBtwSections),
-          itemBuilder: (_, index) => Column(
-            children: [
-              TCartItem(),
-              const SizedBox(height: TSizes.spaceBtwItems),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      /// Extra space:
-                      const SizedBox(width: 70),
 
-                      /// add and remove button:
-                      TProductQuantityWithAddRemoveButton(),
-                    ],
-                  ),
-                  TProductPriceText(price: '256'),
-                ],
-              )
-            ],
-          ),
-        ),
+        ///- Items in cart:
+        child: TCartItems(),
       ),
+
+      /// -- Checkout button:
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
         child: ElevatedButton(onPressed: () {}, child: Text("Checkout \$256")),
---
+++ lib/features/shop/screens/cart/widgets/cart_items.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/products/cart/add_remove_button.dart';
+import 'package:t_store/common/widgets/products/cart/cart_item.dart';
+import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class TCartItems extends StatelessWidget {
+  const TCartItems({
+    super.key,
+    this.showAddRemoveButtons = true,
+  });
+
+  final bool showAddRemoveButtons;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: 15,
+      separatorBuilder: (_, __) => SizedBox(height: TSizes.spaceBtwSections),
+      itemBuilder: (_, index) => Column(
+        children: [
+          /// - Cart item:
+          TCartItem(),
+          if (showAddRemoveButtons)
+            const SizedBox(height: TSizes.spaceBtwItems),
+
+          /// add remove button row with total price:
+          if (showAddRemoveButtons)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    /// Extra space:
+                    const SizedBox(width: 70),
+
+                    /// add remove button:
+                    TProductQuantityWithAddRemoveButton(),
+                  ],
+                ),
+
+                /// -- Product total price:
+                TProductPriceText(price: '256'),
+              ],
+            )
+        ],
+      ),
+    );
+  }
+}
