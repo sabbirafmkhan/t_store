1471430f52394170965de39700d415596883185d
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-05 22:22:08 +0600

    AllBrandsScreen

---
+++ lib/common/widgets/products/sortable/sortable_products.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/layouts/grid_layout.dart';
+import 'package:t_store/common/widgets/products/product_card/product_card_vertical.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class TSortableProducts extends StatelessWidget {
+  const TSortableProducts({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// Dropdown:
+        DropdownButtonFormField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Iconsax.sort),
+          ),
+          onChanged: (value) {},
+          items: [
+            'Name',
+            'Higher Price',
+            'Lower Price',
+            'Sale',
+            'Newest',
+            'Popularity'
+          ]
+              .map((option) =>
+                  DropdownMenuItem(value: option, child: Text(option)))
+              .toList(),
+        ),
+        const SizedBox(height: TSizes.spaceBtwSections),
+
+        /// Products:
+        TGridLayout(
+            itemCount: 10, itemBuilder: (_, index) => TProductCardVertical())
+      ],
+    );
+  }
+}
--- lib/features/shop/screens/brand/brand_products.dart
+++ lib/features/shop/screens/brand/brand_products.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/brands/brand_card.dart';
 import 'package:t_store/common/widgets/products/sortable/sortable_products.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
-class AllProducts extends StatelessWidget {
-  const AllProducts({super.key});
+class BrandProducts extends StatelessWidget {
+  const BrandProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TAppBar(
-        title: Text("Popular Products"),
-        showBackArrow: true,
+        title: Text("Nike"),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(TSizes.defaultSpace),
-          child: TSortableProducts(),
+          child: Column(
+            children: [
+              /// Brand Details:
+              TBrandCard(showBorder: true),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              TSortableProducts(),
+            ],
+          ),
         ),
       ),
     );
