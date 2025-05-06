f77e264dcfaa9a36a15cad3a8ea940c6c454dc71
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-05-06 23:46:19 +0600

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
+
+import 'firebase_options.dart';
 
 /// ------ Entry point of flutter App -------
 
-void main() {
+Future<void> main() async {
   // Todo: Add widgets binding.
   // Todo: init local storage.
   // Todo: Await native splash.
   // Todo: init firebase.
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
   // Todo: init authentication.
 
   runApp(const App());
